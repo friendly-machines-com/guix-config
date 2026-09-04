33e1633e15c92f386a71fdd4f4a81502a0d62dc4
Danny Milosavljevic <dannym@friendly-machines.com>
2026-09-04 21:26:15 +0200

    Add dolphin-emu.

--- home-configuration.scm
+++ home-configuration.scm
@@ -1614,6 +1614,7 @@ protects the token keys by using your system's TPM.  It uses Linux's
              (specification->package "steam")
              (specification->package "heroic") ; system i686-linux
              (specification->package "freeciv")
+             (specification->package "dolphin-emu")
 
 ;;; Office
 
